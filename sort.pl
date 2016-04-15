c930d358b10185503edf8ac8437d1d1f645d6d3a
NeejaYanglea <vero.neeja@gmail.com>
2016-04-15 17:51:56 +0200

    Bug fixes

--- sort.pl
+++ sort.pl
@@ -1,56 +1,53 @@
 /*NAIVE SORT*/
-/*Genera tutte le permutazioni e controlla se sono ordinate*/
-naive_sort(List,Sorted):-perm(List,Sorted),is_sorted(Sorted).
+/*Generates every permutation then checks*/
+naive_sort(List,Sorted) :- perm(List,Sorted),is_sorted(Sorted).
    
 is_sorted([]).
-is_sorted)[_]).
-is_sorted([X,Y|T]):-X=<Y,is_sorted([Y|T]).
+is_sorted([_]).
+is_sorted([X,Y|Rest]) :- X=<Y,is_sorted([Y|Rest]).
 
 /*INSERT SORT*/
-insert_sort(List,Sorted):-i_sort(List,[],Sorted).
+insert_sort(List,Sorted) :- i_sort(List,[],Sorted).
 i_sort([],Acc,Acc).
-i_sort([H|T],Acc,Sorted):-insert(H,Acc,NAcc),i_sort(T,NAcc,Sorted).
+i_sort([H|Rest],Acc,Sorted) :- insert(H,Acc,NAcc),i_sort(Rest,NAcc,Sorted).
    
-insert(X,[Y|T],[Y|NT]):-X>Y,insert(X,T,NT).
-insert(X,[Y|T],[X,Y|T]):-X=<Y.
+insert(X,[Y|Rest],[Y|NRest]) :- X>Y,insert(X,Rest,NRest).
+insert(X,[Y|Rest],[X,Y|Rest]) :- X=<Y.
 insert(X,[],[X]).
 
 /*BUBBLE SORT*/
-bubble_sort(List,Sorted):-b_sort(List,[],Sorted).
+bubble_sort(List,Sorted) :- b_sort(List,[],Sorted).
 b_sort([],Acc,Acc).
-b_sort([H|T],Acc,Sorted):-bubble(H,T,NT,Max),b_sort(NT,[Max|Acc],Sorted).
+b_sort([H|Rest],Acc,Sorted) :- bubble(H,Rest,NRest,Max),b_sort(NRest,[Max|Acc],Sorted).
    
 bubble(X,[],[],X).
-bubble(X,[Y|T],[Y|NT],Max):-X>Y,bubble(X,T,NT,Max).
-bubble(X,[Y|T],[X|NT],Max):-X=<Y,bubble(Y,T,NT,Max).
+bubble(X,[Y|Rest],[Y|NRest],Max) :- X>Y,bubble(X,Rest,NRest,Max).
+bubble(X,[Y|Rest],[X|NRest],Max) :- X=<Y,bubble(Y,Rest,NRest,Max).
 
 /*MERGE SORT*/
-merge_sort([],[]).     % empty list is already sorted
-merge_sort([X],[X]).   % single element list is already sorted
-merge_sort(List,Sorted):-
+merge_sort([],[]).    
+merge_sort([X],[X]).   
+merge_sort(List,Sorted) :- 
     List=[_,_|_],divide(List,L1,L2),     % list with at least two elements is divided into two parts
-	merge_sort(L1,Sorted1),merge_sort(L2,Sorted2),  % then each part is sorted
-	merge(Sorted1,Sorted2,Sorted).                  % and sorted parts are merged
+	merge_sort(L1,Sorted1),merge_sort(L2,Sorted2),  
+	merge(Sorted1,Sorted2,Sorted).                  
 merge([],L,L).
-merge(L,[],L):-L\=[].
-merge([X|T1],[Y|T2],[X|T]):-X=<Y,merge(T1,[Y|T2],T).
-merge([X|T1],[Y|T2],[Y|T]):-X>Y,merge([X|T1],T2,T).
+merge(L,[],L) :- L\=[].
+merge([X|Rest1],[Y|Rest2],[X|Rest]) :- X=<Y,merge(Rest1,[Y|Rest2],Rest).
+merge([X|Rest1],[Y|Rest2],[Y|Rest]) :- X>Y,merge([X|Rest1],Rest2,Rest).
 
-divide(L,L1,L2):-halve(L,L1,L2).
+divide(L,L1,L2) :- append(L1, L2, L),
+    length(L1, N),
+    length(L2, M),
+    (M=N ; M is N-1).
 
 /*QUICK SORT*/
-quick_sort([],[]).
-quick_sort([H|T],Sorted):-
-	pivoting(H,T,L1,L2),quick_sort(L1,Sorted1),quick_sort(L2,Sorted2),
-	append(Sorted1,[H|Sorted2]).
-   
 pivoting(H,[],[],[]).
-pivoting(H,[X|T],[X|L],G):-X=<H,pivoting(H,T,L,G).
-pivoting(H,[X|T],L,[X|G]):-X>H,pivoting(H,T,L,G).
+pivoting(H,[X|Rest],[X|L],G) :- X>H,pivoting(H,Rest,L,G).
+pivoting(H,[X|Rest],L,[X|G]) :- X=<H,pivoting(H,Rest,L,G).
 
-
-quick_sort2(List,Sorted):-q_sort(List,[],Sorted).
+quick_sort(List,Sorted) :- q_sort(List,[],Sorted).
 q_sort([],Acc,Acc).
-q_sort([H|T],Acc,Sorted):-
-	pivoting(H,T,L1,L2),
-	q_sort(L1,Acc,Sorted1),q_sort(L2,[H|Sorted1],Sorted)
+q_sort([H|Rest],Acc,Sorted) :- 
+	pivoting(H,Rest,L1,L2),
+	q_sort(L1,Acc,Sorted1),q_sort(L2,[H|Sorted1],Sorted).
